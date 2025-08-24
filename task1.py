0778c5bed0c5413213855c697a45df3da119eba5
Rohit <rohit.chandra96@gmail.com>
2025-08-24 14:51:11 +0530

    Added task1.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=int(input('Enter the first number: '))
+b=int(input('Enter the second number: '))
+print('The first number: ', a)
+print('The second number: ', b)
+c=a+b
+print('Addition: ', c)
+c=a-b
+print('Subtraction: ',c)
+c=a*b
+print('Multiplication: ',c)
+c=a/b
+print('Division: ',c) 
