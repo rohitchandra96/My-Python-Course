128e31ca10066b6aae63671536d2f8abf25fcf2b
Rohit <rohit.chandra96@gmail.com>
2025-08-28 21:15:21 +0530

    Updated task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,20 @@
-a=input('Enter your first name: ')
-b= input('Enter your last name: ')
-c= a+" "+b
-#print(c)
-print('Hello, '+ c + '! Welcome to the Python Program')
+#Sum of Integers from 1 to 50 Using a Loop
+a = list(range(1,50+1))
+#print(a)
+sum=0
+for i in a:
+    sum += i 
+print("The sum of numbers from 1 to 50 is: ", sum)
+
+
+
+
+
+
+#Old Task
+
+# a=input('Enter your first name: ')
+# b= input('Enter your last name: ')
+# c= a+" "+b
+# #print(c)
+# print('Hello, '+ c + '! Welcome to the Python Program')
