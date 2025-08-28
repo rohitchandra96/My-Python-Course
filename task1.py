e0323671a8dc29e2330d6f69c5db19f6a6662adb
Rohit <rohit.chandra96@gmail.com>
2025-08-28 20:34:56 +0530

    Added task1.py

--- task1.py
+++ task1.py
@@ -1,12 +1,33 @@
-a=int(input('Enter the first number: '))
-b=int(input('Enter the second number: '))
-print('The first number: ', a)
-print('The second number: ', b)
-c=a+b
-print('Addition: ', c)
-c=a-b
-print('Subtraction: ',c)
-c=a*b
-print('Multiplication: ',c)
-c=a/b
-print('Division: ',c) 
+# Check if a Number is Even or Odd
+a=int(input("Enter a number: "))
+if a%2==0:
+    print(a, " is an even number")
+else:
+    print(a, "is an odd number")
+
+
+
+
+
+
+
+
+
+
+
+#Old Task
+# a=int(input('Enter the first number: '))
+# b=int(input('Enter the second number: '))
+# print('The first number: ', a)
+# print('The second number: ', b)
+# c=a+b
+# print('Addition: ', c)
+# c=a-b
+# print('Subtraction: ',c)
+# c=a*b
+# print('Multiplication: ',c)
+# c=a/b
+# print('Division: ',c) 
+
+
+
